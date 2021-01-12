0f56193c0aca2ac6b98ab8f6468813fd8ef0c3f4
Renan Luiz <renanllm01@gmail.com>
2021-01-11 21:45:55 -0300

    Main commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,21 @@
+import { v4 as uuidv4 } from 'uuid';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface BankStatement {
+  transactions: Transaction[];
+  balance: Balance;
+}
+
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
 }
 
 class TransactionsRepository {
@@ -13,16 +25,32 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): BankStatement {
+    const bankStatement = {
+      transactions: this.transactions,
+      balance: this.getBalance(),
+    };
+    return bankStatement;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance: Balance = { income: 0, outcome: 0, total: 0 };
+
+    this.transactions.forEach(transaction => {
+      balance[transaction.type] += transaction.value;
+    });
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ type, value, title }: TransactionDTO): Transaction {
+    const transaction = { id: uuidv4(), title, type, value };
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
